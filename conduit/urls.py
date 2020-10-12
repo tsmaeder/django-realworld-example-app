0d425480c1b28d6063c255e7c4fa869eb1709d85
Thomas Mäder <tmader@redhat.com>
2020-10-12 12:50:28 +0000

    Update deps to run on python 3.8.6
    Signed-off-by: Thomas Mäder <tmader@redhat.com>

--- conduit/urls.py
+++ conduit/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
 
-    url(r'^api/', include('conduit.apps.articles.urls', namespace='articles')),
-    url(r'^api/', include('conduit.apps.authentication.urls', namespace='authentication')),
-    url(r'^api/', include('conduit.apps.profiles.urls', namespace='profiles')),
+    url(r'^api/', include(('conduit.apps.articles.urls', 'articles'), namespace='articles')),
+    url(r'^api/', include(('conduit.apps.authentication.urls', 'authentication'), namespace='authentication')),
+    url(r'^api/', include(('conduit.apps.profiles.urls', 'profiles'), namespace='profiles'))
 ]
